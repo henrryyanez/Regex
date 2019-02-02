87f14ef08bd69e86cf95df26ee1d4b9bb3fa9a98
hy <henrryyanez@gmail.com>
2019-02-01 21:22:58 -0300

    Codding

--- validadorUrlsconImagenes.py
+++ validadorUrlsconImagenes.py
@@ -1,1 +1,11 @@
-(.*)([\/](\w+))(\.(jpg|png|gif|jpeg))
+  import re
+
+#Validamos las urls con http contenidas en un logs
+
+txt = "The rain in Spain"
+x = re.search("(.*)([\/](\w+))(\.(jpg|png|gif|jpeg))", txt)
+
+if (x):
+  print("SI! Coincide!")
+else:
+  print("No coincide")
