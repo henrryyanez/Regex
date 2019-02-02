04b5fbb4170b71126cc52c32a5cfb502f6e71d76
hy <henrryyanez@gmail.com>
2019-02-01 21:20:12 -0300

    Codigo Py completo

--- validadorURL.py
+++ validadorURL.py
@@ -1,1 +1,11 @@
-http:\/\/(www\.[a-z0-9]+([\-\.][a-z0-9]+)*\.[a-z]{2,5})(:[0-9]{1,5})?(\/.*)?$|^(\/.*)
+  import re
+
+#Validamos las urls con http contenidas en un logs
+
+txt = "The rain in Spain"
+x = re.search("http:\/\/(www\.[a-z0-9]+([\-\.][a-z0-9]+)*\.[a-z]{2,5})(:[0-9]{1,5})?(\/.*)?$|^(\/.*)", txt)
+
+if (x):
+  print("SI! Coincide!")
+else:
+  print("No coincide")
